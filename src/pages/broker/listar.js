e32b9f6a96ea4ca232b3c00276a93a38197e3f93
Maria Clara Lacerda <104872830+MariaLac@users.noreply.github.com>
2023-10-04 20:31:31 -0300

    Update listar.js

--- src/pages/broker/listar.js
+++ src/pages/broker/listar.js
@@ -1,27 +1,24 @@
 import ListarDispositivo from "../components/ListarDispositivo"
 import NavbarPrincipal from "../components/NavbarPrincipal"
- telas-broker
-
 import styles from '@/styles/Broker.module.css'
- main
 
 export default function PaginaListar(){
     return(
     <>
     <NavbarPrincipal/>
-telas-broker
+
     <fieldset className="fundolistar">
         <fieldset className="linhastabela">
 
     <div className={styles.broker}>
     <fieldset className={styles.fundolistar}>
         <fieldset className={styles.linhastabela}>
- main
+ 
             <ListarDispositivo />        
             <ListarDispositivo />        
             <ListarDispositivo />        
             <ListarDispositivo />        
- telas-broker
+ 
             <div className="deseja"><a href="cadastrar">Deseja adicionar um novo dispositivo?</a></div>
         </fieldset>
     </fieldset>
@@ -30,7 +27,6 @@ telas-broker
         </fieldset>
     </fieldset>
     </div>
- main
     </>
     )
 }
