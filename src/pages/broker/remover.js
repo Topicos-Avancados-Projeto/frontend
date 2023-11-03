9f9cc30e824b2892039544e9c2a4ef2eeeca5df8
Maria Clara Lacerda <mcal1@discente.ifpe.edu.br>
2023-11-03 15:43:41 -0300

    front integrado

--- src/pages/broker/remover.js
+++ src/pages/broker/remover.js
@@ -1,31 +1,25 @@
-import ListarDispositivo from "../components/ListarDispositivo"
-import NavbarPrincipal from "../components/NavbarPrincipal"
-import styles from '@/styles/Broker.module.css'
+import ListarDispositivo from "../components/ListarDispositivo";
+import NavbarPrincipal from "../components/NavbarPrincipal";
+import styles from '@/styles/Broker.module.css';
 
-export default function PaginaRemover(){
-    return(
-    <><NavbarPrincipal/>
-    <fieldset className="fundolistar">
-        <fieldset className="linhastabela">
-            <ListarDispositivo />        
-            <ListarDispositivo />        
-            <ListarDispositivo />        
-            <ListarDispositivo />        
-            <div className="deseja"><a href="cadastrar">Deseja adicionar um novo dispositivo?</a></div>
-        </fieldset>
-    </fieldset></>
-
-    <div className={styles.broker}>
-        <fieldset className={styles.fundolistar}>
-            <fieldset className={styles.linhastabela}>
-                <ListarDispositivo />        
-                <ListarDispositivo />        
-                <ListarDispositivo />        
-                <ListarDispositivo />        
-                <div className={styles.deseja}><a href="cadastrar">Deseja adicionar um novo dispositivo?</a></div>
-            </fieldset>
-        </fieldset>
-    </div>
+export default function PaginaRemover() {
+  return (
+    <>
+      <NavbarPrincipal />
+      <div className={styles.broker}>
+        <div className={styles.fundolistar}>
+          <div className={styles.linhastabela}>
+            <ListarDispositivo />
+            <ListarDispositivo />
+            <ListarDispositivo />
+            <ListarDispositivo />
+            <div className={styles.deseja}>
+              <a href="cadastrar">Deseja adicionar um novo dispositivo?</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
+
--- src/pages/components/ListarDispositivo.js
+++ src/pages/components/ListarDispositivo.js
@@ -1,18 +1,8 @@
- telas-broker
 export default function ListarDispositivo(){
     return(
         <fieldset className="divs">
                 <div>
                     <label className="nomes">Dispositivo ux-776</label>
-
-import styles from '@/styles/Broker.module.css'
-
-export default function ListarDispositivo(){
-    return(
-        <fieldset className={styles.divs}>
-                <div>
-                    <label className={styles.nomes}>Dispositivo ux-776</label>
- main
                 </div>
                 <div>
                     <button><img src="/images/lixo.png" /></button>
