a27b1b8a9df7ba409494bc1701d8e353986528c7
pedrowata <142267232+pedrowata@users.noreply.github.com>
2023-11-07 16:49:47 -0300

    Integração - broker - telas listar e remover

--- src/pages/broker/listar.js
+++ src/pages/broker/listar.js
@@ -1,27 +1,16 @@
 import ListarDispositivo from "../components/ListarDispositivo";
 import NavbarPrincipal from "../components/NavbarPrincipal";
-import styles from '@/styles/Broker.module.css'
+import styles from '@/styles/Listar.module.css'
 
 export default function PaginaListar() {
   return (
     <>
       <NavbarPrincipal />
-      <fieldset className="fundolistar">
-        <fieldset className="linhastabela">
-          <div className={styles.broker}>
-            <fieldset className={styles.fundolistar}>
-              <fieldset className={styles.linhastabela}>
-                <ListarDispositivo />
-                <ListarDispositivo />
-                <ListarDispositivo />
-                <ListarDispositivo />
-                <div className="deseja">
-                  <a href="cadastrar">Deseja adicionar um novo dispositivo?</a>
-                </div>
-              </fieldset>
-            </fieldset>
+      <fieldset className={styles.listar}>
+          <ListarDispositivo/>
+          <div>
+            <a href="cadastrar">Deseja adicionar um novo dispositivo?</a>
           </div>
-        </fieldset>
       </fieldset>
     </>
   );
